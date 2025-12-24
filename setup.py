89ab5099ccfe3de806ad0d01fbef70f6a4bd3405
Jason.Lin <jolinjo@gmail.com>
2025-12-24 11:10:27 +0800

    更新版本號為 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='Project-Archiver',
-    version='0.1.0',
+    version='0.1.1',
     description='A Project Archiving Utility for Fusion 360',
     long_description=long_description,
     packages=['Project-Archiver', 'Project-Archiver.apper.apper', 'Project-Archiver.commands'],
